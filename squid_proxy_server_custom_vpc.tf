9068a1667837bc9b2a7be5014677e9d22a09bdae
CottonPajamas <30998454+CottonPajamas@users.noreply.github.com>
2020-07-10 15:35:26 +0800

    Create squid_proxy_server_custom_vpc.tf

---
+++ squid_proxy_server_custom_vpc.tf
@@ -0,0 +1,8 @@
+# Creating a squid forward proxy server within a custom VPC.
+
+provider "aws" {
+	access_key 	= "<<specify access_key>>"
+	secret_key 	= "<<specify secret_key>>"
+	region 		= "<<specify region>>"
+	version		= "2.7"
+}
