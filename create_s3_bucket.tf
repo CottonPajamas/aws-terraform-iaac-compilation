5ef7d73237ec0e82f02493901fbad26a27db837c
CottonPajamas <30998454+CottonPajamas@users.noreply.github.com>
2020-07-10 14:35:06 +0800

    Update create_s3_bucket.tf

--- create_s3_bucket.tf
+++ create_s3_bucket.tf
@@ -12,34 +12,34 @@ provider "aws" {
 
 # Creating S3 bucket
 resource "aws_s3_bucket" "custom_bucket" {
-  bucket = "my-custom-s3-terraform-bucket"
-  acl = "private"
+	bucket = "my-custom-s3-terraform-bucket"
+	acl	= "private"
 
 	# Enables versioning
-  versioning {
-    enabled = true
-  }
+	versioning {
+		enabled = true
+	}
 
-  tags = {
-    Name = "my-custom-s3-terraform-bucket"
-    Environment = "dev"
-  }
+	tags = {
+		Name = "my-custom-s3-terraform-bucket"
+		Environment = "Dev"
+	}
 }
 
 # Blocks all public access for our newly created bucket - this can be found under the bucket's 'Permssions' settings
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = aws_s3_bucket.custom_bucket.id
-  block_public_acls   		= true
-  block_public_policy 		= true
-	ignore_public_acls 			= true
-	restrict_public_buckets = true
+	bucket = aws_s3_bucket.custom_bucket.id
+	block_public_acls				=	true
+	block_public_policy			=	true
+	ignore_public_acls			=	true
+	restrict_public_buckets	=	true
 }
 
 # Generates output
 output "sg_id" {
-  value = aws_s3_bucket.custom_bucket.id
+	value = aws_s3_bucket.custom_bucket.id
 }
 
 output "sg_bucket_domain_name" {
-  value = aws_s3_bucket.custom_bucket.bucket_domain_name
+	value = aws_s3_bucket.custom_bucket.bucket_domain_name
 }
