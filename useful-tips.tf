a6af609bb305fa6915146f129d7614ed9d920acd
CottonPajamas <30998454+CottonPajamas@users.noreply.github.com>
2020-10-05 19:10:52 +0800

    Update useful-tips.tf

--- useful-tips.tf
+++ useful-tips.tf
@@ -11,8 +11,6 @@ resource "aws_instance" "server" {
 }
 
 
-
-
 # 2. Creating multiple resources
 # Ref: https://www.bogotobogo.com/DevOps/Terraform/Terraform-Introduction-AWS-loops.php#for
 variable "qty" {
@@ -26,5 +24,35 @@ resource "aws_instance" "server" {
   instance_type = "t2.nano"
   tags = {
     Name = "Server ${count.index}"
+  }
+}
+
+
+# 3. Using the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon-linux-2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+
+# 4. Using the latest Ubuntu 18.04 Bionic AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
